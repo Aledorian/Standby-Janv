e4648eaf8b42bbc68c583b3eea4d07a50f4176dd
simplon <aledorianlegrand@gmail.com>
2017-01-19 16:59:08 +0100

    Correction 2bis

--- js/script.js
+++ js/script.js
@@ -9,14 +9,14 @@
 
 function empty() {
 
-    y = document.getElementById("prenom").value;
-    if (y == "") {
-        document.getElementById("pasprenom").innerHTML = "Entrer votre prenom !";
-        document.getElementById("prenom").style.borderBottomColor = "#880000";
-    } else {
-        document.getElementById('pasprenom').innerHTML = "";
-        document.getElementById('prenom').style.borderBottomColor = "rgb(82, 144, 249)"
-    }
+    // y = document.getElementById("prenom").value;
+    // if (y == "") {
+    //     document.getElementById("pasprenom").innerHTML = "Entrer votre prenom !";
+    //     document.getElementById("prenom").style.borderBottomColor = "#880000";
+    // } else {
+    //     document.getElementById('pasprenom').innerHTML = "";
+    //     document.getElementById('prenom').style.borderBottomColor = "rgb(82, 144, 249)"
+    // }
 
     a = document.getElementById("telephone").value;
     if (a == "") {
@@ -76,5 +76,19 @@ function checkName() {
     else {
       document.getElementById('pasnom').innerHTML = "";
       document.getElementById('nom').style.borderBottomColor = "rgb(82, 144, 249)"
+    }
+
+    var fstr = $('#prenom').val();
+    if (/^[a-zA-Z0-9- ]*$/.test(fstr) == false ) {
+        document.getElementById("pasprenom").innerHTML = "Caractère incorecte dans votre prénom.";
+        document.getElementById("prenom").style.borderBottomColor = "#880000";
+    }
+    else if (fstr == "") {
+      document.getElementById("pasprenom").innerHTML = "Entrez votre nom !.";
+      document.getElementById("prenom").style.borderBottomColor = "#880000";
+    }
+    else {
+      document.getElementById('pasprenom').innerHTML = "";
+      document.getElementById('prenom').style.borderBottomColor = "rgb(82, 144, 249)"
     }
 }
