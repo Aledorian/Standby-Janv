10e4af8b41098faa2794c1f26829b230bef59881
simplon <aledorianlegrand@gmail.com>
2017-03-07 01:01:13 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+function checkEmail() {
+  var status = false;
+  var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+  if (document.getElementById("mail").value.search(emailRegEx) == -1) {
+    document.getElementById("pasmail").innerHTML = "Entrer une adresse mail valide !";
+    document.getElementById("mail").style.borderBottomColor = "#880000";
+  } else {
+    status = true;
+    document.getElementById("pasmail").innerHTML = "";
+    document.getElementById("mail").style.borderBottomColor = "rgb(82, 144, 249)";
+
+  }
+  return status;
+}
+
+
 function empty() {
 
     a = document.getElementById("telephone").value;
@@ -7,16 +24,6 @@ function empty() {
     } else {
         document.getElementById('pasnum').innerHTML = "";
         document.getElementById('telephone').style.borderBottomColor = "rgb(82, 144, 249)"
-    }
-
-
-    b = document.getElementById("mail").value;
-    if (b == "") {
-        document.getElementById("pasmail").innerHTML = "Entrer votre adresse mail !";
-        document.getElementById("mail").style.borderBottomColor = "#880000";
-    } else {
-        document.getElementById('pasmail').innerHTML = "";
-        document.getElementById('mail').style.borderBottomColor = "rgb(82, 144, 249)"
     }
 
     c = document.getElementById("message").value;
@@ -29,30 +36,16 @@ function empty() {
     }
 }
 
-function checkEmail() {
-    var status = false;
-    var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    if (document.getElementById("mail").value.search(emailRegEx) == -1) {
-        document.getElementById("pasmail").innerHTML = "Entrer une adresse mail valide.";
-        document.getElementById("mail").style.borderBottomColor = "#880000";
-    } else {
-        status = true;
-        document.getElementById("pasmail").innerHTML = "";
-        document.getElementById("mail").style.borderBottomColor = "rgb(82, 144, 249)";
-
-    }
-    return status;
-}
 
 function checkName() {
 
     var str = $('#nom').val();
-    if (/^[a-zA-Z0-9- ]*$/.test(str) == false ) {
+    if (/^[a-zA-Z]*$/.test(str) == false ) {
         document.getElementById("pasnom").innerHTML = "Caractère incorecte dans votre nom.";
         document.getElementById("nom").style.borderBottomColor = "#880000";
     }
     else if (str == "") {
-      document.getElementById("pasnom").innerHTML = "Entrez votre nom !.";
+      document.getElementById("pasnom").innerHTML = "Entrez votre nom !";
       document.getElementById("nom").style.borderBottomColor = "#880000";
     }
     else {
@@ -61,18 +54,34 @@ function checkName() {
     }
 
     var fstr = $('#prenom').val();
-    if (/^[a-zA-Z0-9- ]*$/.test(fstr) == false ) {
+    if (/^[a-zA-Z-]*$/.test(fstr) == false ) {
         document.getElementById("pasprenom").innerHTML = "Caractère incorecte dans votre prénom.";
         document.getElementById("prenom").style.borderBottomColor = "#880000";
     }
     else if (fstr == "") {
-      document.getElementById("pasprenom").innerHTML = "Entrez votre nom !.";
+      document.getElementById("pasprenom").innerHTML = "Entrez votre nom !";
       document.getElementById("prenom").style.borderBottomColor = "#880000";
     }
     else {
       document.getElementById('pasprenom').innerHTML = "";
       document.getElementById('prenom').style.borderBottomColor = "rgb(82, 144, 249)"
     }
+}
+
+function checkNum() {
+  // var statu = false;
+  // var telRegEx = /[0-9]{10}/;
+  //
+  // if (documesnt.getElementById("telephone").value.search(telRegEx) == -1) {
+  //   document.getElementById("pasnum").innerHTML = "Entrer une adresse mail valide !";
+  //   document.getElementById("telephone").style.borderBottomColor = "#880000";
+  // } else {
+  //   status = true;
+  //   document.getElementById("pasnum").innerHTML = "";
+  //   document.getElementById("telephone").style.borderBottomColor = "rgb(82, 144, 249)";
+  // }
+  // return status;
+  alert('Test');
 }
 
 $(function() {
